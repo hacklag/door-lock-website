1764a1eae778741649ca8f8b0239e1910769d225
Dawid Kozak <d.kozak92@gmail.com>
2016-05-18 11:54:03 +0200

    [HAC-238] Add spinner and spinner logic

--- src/apps/Landing/Landing.js
+++ src/apps/Landing/Landing.js
@@ -8,23 +8,30 @@ export default Radium(React.createClass({
 
   getInitialState() {
     return {
-      message: null
+      message: null,
+      loading: false
     };
   },
 
   componentWillMount() {
-    const connection = Syncano({accountKey: 'api-key'});
+    const connection = Syncano({accountKey: 'accoutKey'});
     const Channel = connection.Channel;
     const query = {
-      name: 'channel-name',
-      instanceName: 'instance-name'
+      name: 'channelName',
+      instanceName: 'instanceName'
     };
     const poll = Channel.please().poll(query);
 
     poll.on('update', (data) => {
+      let loadingVar = false;
+
+      if (data.payload.status === 'loading') {
+        loadingVar = true;
+      }
       console.log(data.payload);
       this.setState({
-        message: data.payload.message
+        message: data.payload.message,
+        loading: loadingVar
       });
     });
 
@@ -52,25 +59,42 @@ export default Radium(React.createClass({
       },
       contentBar: {
         marginLeft: 72,
+        marginTop: 40,
         '@media (max-width: 750px)': {
           marginLeft: 'auto',
           marginRight: 'auto',
           padding: '0px 16px 16px 16px'
         }
+      },
+      fullScreenComponent: {
+        position: 'relative'
+      },
+      centerLoader: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        margin: '-50px 0px 0px -50px'
       }
     };
   },
 
   contentForm() {
     const styles = this.getStyles();
+    const loading = this.state.loading;
     let message = this.state.message;
 
     if (!message) message = 'Hello Stranger';
     return (
-      <div style={styles.content}>
-        <div style={styles.mainTextContainer}>
-          <div style={styles.headerText}>
-            <span style={styles.semiBold}>{message}</span>
+      <div>
+        <div style={styles.content}>
+          <div style={styles.mainTextContainer}>
+            <div style={styles.headerText}>
+              {
+                loading
+                ? (<img src={'/img/loader.gif'} />)
+                : (<span style={styles.semiBold}>{message}</span>)
+              }
+            </div>
           </div>
         </div>
       </div>
